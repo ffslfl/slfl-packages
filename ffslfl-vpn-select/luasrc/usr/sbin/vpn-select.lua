625dda5cb16a91e7e0d2382006e6f05d81361b37
Marcel Radzio <mtrnord1@gmail.com>
2017-05-22 11:17:09 +0200

    Update more for lua
    Signed-off-by: Marcel <mtrnord1@gmail.com>

--- ffslfl-vpn-select/luasrc/usr/sbin/vpn-select.lua
+++ ffslfl-vpn-select/luasrc/usr/sbin/vpn-select.lua
@@ -5,6 +5,22 @@ local util = require 'gluon.util'
 local fs = require 'nixio.fs'
 
 local uci = require('simple-uci').cursor()
+--- Check if a file or directory exists in this path
+function exists(file)
+    local ok, err, code = os.rename(file, file)
+    if not ok then
+        if code == 13 then
+            -- Permission denied, but it exists
+            return true
+        end
+    end
+    return ok, err
+end
+
+--- Check if a directory exists in this path
+function isdir(path)
+    return exists(path.."/")
+end
 
 uci:section('firewall', 'include', 'mesh_vpn_dns', {
     type = 'restore',
@@ -16,11 +32,38 @@ local mac_raw = util.trim(fs.readfile('/sys/class/net/br-wan/address'))
 local mac = mac_raw.gsub(":", "").upper
 local hostname = util.trim(fs.readfile('/proc/sys/kernel/hostname'))
 local port = ''
-local pubkey = ''
+local pubkey = uci:get("fastd", "mesh_vpn", "pubkey")
 local lat = uci:get_first('gluon-node-info', 'location', 'latitude')
 local long = uci:get_first('gluon-node-info', 'location', 'longitude')
 
+if pubkey == '' then
+    io.popen('uci set fastd.mesh_vpn.pubkey="secret \"$(uci get fastd.mesh_vpn.secret)\";" | fastd -c - --show-key --machine-readable')
+    io.popen('uci commit fastd')
+    pubkey = uci:get("fastd", "mesh_vpn", "pubkey")
+end
+
+if isdir('/tmp/fastd_mesh_vpn_peers') then
+    -- WOOOOW
+else
+    fs.mkdir('/tmp/fastd_mesh_vpn_peers')
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 io.popen('wget -4 -T15 "http://keyserver.ffslfl.net/index.php?mac=' .. mac .. '&name=' .. hostname .. '&port=' .. port .. '&key=' .. pubkey.. '&lat=' .. lat .. '&long=' .. long.. ' -O /tmp/fastd_ffslfl_output ', 'r')
+
+
 
 local enabled = uci:get('tunneldigger', 'mesh_vpn', 'enabled')
 if enabled == nil then
