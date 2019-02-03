d44938ec393cc5145f91c1a9c3ef3bde568d34d8
MTRNord <mtrnord1@gmail.com>
2019-02-03 13:57:36 +0100

    Change groupid of process on start
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

--- ffslfl-vpn-select/src/vpn_select.c
+++ ffslfl-vpn-select/src/vpn_select.c
@@ -1,1 +1,32 @@
-int main(int argc, char *argv[]) {}
+#include <unistd.h>
+#include <sys/types.h>
+#include <grp.h>
+
+void moveToCorrectProcess() {
+  struct group *grp;
+
+  grp = getgrnam("gluon-mesh-vpn");
+  if (grp == NULL) {
+    /// "Failed to get gid"
+  }
+
+  setpgid(0, grp->gr_gid);
+}
+
+void run() {
+
+}
+
+int main(int argc, char *argv[]) {
+  int f = fork();
+  if(f < 0)
+    perror("fork error:");
+  else if(f == 0) /* child */
+  {
+    moveToCorrectProcess();
+
+    // RUN Script
+    run();
+  }
+
+}
